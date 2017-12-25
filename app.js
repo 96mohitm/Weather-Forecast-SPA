3ac253ba816c731113ad9aefec3d5910ae03a194
Mohit Musaddi <96mohitm@gmail.com>
2017-12-25 13:48:42 +0530

    Add fahrenheit function.

--- app.js
+++ app.js
@@ -34,23 +34,24 @@ function($scope, cityService) {
     
 }]);
 //FORECAST CONTROLLER
-weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService',
-function($scope, $resource, cityService) {
-//    
-//    $scope.city = cityService.city;
-//    
-//    $scope.weatherAPI = $resource("https://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" } });
-//    
-////    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2, appid: 'd42fef1a17e7a422d8a3c6e40e283ba9' });
-//    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 6, appid: 'd42fef1a17e7a422d8a3c6e40e283ba9' });
-//    console.log($scope.weatherResult);
+weatherApp.controller('forecastController', ['$scope', '$resource', '$log', 'cityService',
+function($scope, $resource, $log, cityService) {
     $scope.name = 'Forecast';
     $scope.city = cityService.city;
-    $scope.days = 6;
     $scope.weatherAPI = $resource("https://api.openweathermap.org/data/2.5/forecast/daily", { callback: "JSON_CALLBACK" }, { get: { method: "JSONP" } });
 
-    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days, appid: '273783dc29c87c3659c1ee5076017988' });
-    console.log($scope.weatherResult);
-}]);
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2, appid: 'ae234bf2fbf8fced58092ee511d18d09' });
 
+    $scope.covertToFahrenheit = function(degK) {
+        return Math.round((1.8 * (degK - 273)) + 32);
+    };
+
+
+
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    });
+
+}]);
+//console.log($scope);
 //d42fef1a17e7a422d8a3c6e40e283ba9
